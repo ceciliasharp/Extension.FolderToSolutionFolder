ecdc1217d6fdf005f2c5c71add4784e2a8ffd700
LoewT <Tobias.Loew@steag.com>
2021-03-13 10:58:45 +0100

    udated resources added status messages fixed adding to solution-folder

--- FolderToSolutionFolder/FolderCommand.cs
+++ FolderToSolutionFolder/FolderCommand.cs
@@ -30,6 +30,7 @@ namespace CeciliaSharp.FolderToSolutionFolder
         /// Command menu group (command set GUID).
         /// </summary>
         public static readonly Guid CommandSetSolution = new Guid("7fd09a49-455e-442f-921e-c2c4d5e12997");
+        public static readonly Guid CommandSetSolutionFolder = new Guid("7fd09a49-455e-442f-921e-c2c4d5e12999");
         public static readonly Guid CommandSetProject = new Guid("7fd09a49-455e-442f-921e-c2c4d5e12998");
 
         /// <summary>
@@ -52,12 +53,17 @@ namespace CeciliaSharp.FolderToSolutionFolder
             {
                 {
                     var menuCommandID = new CommandID(CommandSetSolution, CommandId);
-                    var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                    var menuItem = new MenuCommand(this.MenuItemCallbackAddToSolution, menuCommandID);
+                    commandService.AddCommand(menuItem);
+                }
+                {
+                    var menuCommandID = new CommandID(CommandSetSolutionFolder, CommandId);
+                    var menuItem = new MenuCommand(this.MenuItemCallbackAddToSolutionFolder, menuCommandID);
                     commandService.AddCommand(menuItem);
                 }
                 {
                     var menuCommandID = new CommandID(CommandSetProject, CommandId);
-                    var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                    var menuItem = new MenuCommand(this.MenuItemCallbackAddToProject, menuCommandID);
                     commandService.AddCommand(menuItem);
                 }
             }
@@ -94,6 +100,28 @@ namespace CeciliaSharp.FolderToSolutionFolder
             Instance = new FolderCommand(package);
         }
 
+        enum DestinationType
+        {
+            Solution,
+            SolutionFolder,
+            Project
+        }
+
+        private void MenuItemCallbackAddToSolution(object sender, EventArgs e)
+        {
+            MenuItemCallback(DestinationType.Solution);
+        }
+
+        private void MenuItemCallbackAddToSolutionFolder(object sender, EventArgs e)
+        {
+            MenuItemCallback(DestinationType.SolutionFolder);
+        }
+
+        private void MenuItemCallbackAddToProject(object sender, EventArgs e)
+        {
+            MenuItemCallback(DestinationType.Project);
+        }
+
         /// <summary>
         /// This function is the callback used to execute the command when the menu item is clicked.
         /// See the constructor to see how the menu item is associated with this function using
@@ -101,76 +129,135 @@ namespace CeciliaSharp.FolderToSolutionFolder
         /// </summary>
         /// <param name="sender">Event sender.</param>
         /// <param name="e">Event args.</param>
-        private void MenuItemCallback(object sender, EventArgs e)
+        private void MenuItemCallback(DestinationType destination)
         {
             string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
 
-            // figure out if a project or project-folder was clicked
-            ProjectItems items = null;
+            ProjectItems rootItems = null;
+
+            switch (destination)
+            {
+                case DestinationType.Solution:
+                    {
+                    }
+                    break;
+
+                case DestinationType.SolutionFolder:
+                case DestinationType.Project:
+                    {
+                        // figure out if a project or project-folder was clicked
+                        try
+                        {
+                            IntPtr hierarchyPointer, selectionContainerPointer;
+                            Object selectedObject = null;
+                            IVsMultiItemSelect multiItemSelect;
+                            uint projectItemId;
+
+                            IVsMonitorSelection monitorSelection =
+                                    (IVsMonitorSelection)Package.GetGlobalService(
+                                    typeof(SVsShellMonitorSelection));
+
+                            monitorSelection.GetCurrentSelection(out hierarchyPointer,
+                                                                 out projectItemId,
+                                                                 out multiItemSelect,
+                                                                 out selectionContainerPointer);
+
+                            IVsHierarchy selectedHierarchy = Marshal.GetTypedObjectForIUnknown(
+                                                                 hierarchyPointer,
+                                                                 typeof(IVsHierarchy)) as IVsHierarchy;
+
+                            if (selectedHierarchy != null)
+                            {
+                                ErrorHandler.ThrowOnFailure(selectedHierarchy.GetProperty(
+                                                                  projectItemId,
+                                                                  (int)__VSHPROPID.VSHPROPID_ExtObject,
+                                                                  out selectedObject));
+                            }
+
+                            dynamic dyn = selectedObject;
+                            rootItems = (ProjectItems)(dyn.ProjectItems);
+                        }
+                        catch (Exception)
+                        {
+                            // what the heck was clicked ???
+                        }
+
+                    }
+                    break;
+
+            }
+
+            string basePath;
+            switch (destination)
+            {
+                default:
+                case DestinationType.Solution:
+                case DestinationType.SolutionFolder:
+                    {
+                        Solution2 sol2 = (Solution2)dte.Solution;
+                        basePath = Path.GetDirectoryName(sol2.FullName);
+                    }
+                    break;
+
+                case DestinationType.Project:
+                    {
+                        basePath = Path.GetDirectoryName(rootItems.ContainingProject.FullName);
+                    }
+                    break;
+            }
+
             try
             {
-                IntPtr hierarchyPointer, selectionContainerPointer;
-                Object selectedObject = null;
-                IVsMultiItemSelect multiItemSelect;
-                uint projectItemId;
 
-                IVsMonitorSelection monitorSelection =
-                        (IVsMonitorSelection)Package.GetGlobalService(
-                        typeof(SVsShellMonitorSelection));
-
-                monitorSelection.GetCurrentSelection(out hierarchyPointer,
-                                                     out projectItemId,
-                                                     out multiItemSelect,
-                                                     out selectionContainerPointer);
-
-                IVsHierarchy selectedHierarchy = Marshal.GetTypedObjectForIUnknown(
-                                                     hierarchyPointer,
-                                                     typeof(IVsHierarchy)) as IVsHierarchy;
-
-                if (selectedHierarchy != null)
-                {
-                    ErrorHandler.ThrowOnFailure(selectedHierarchy.GetProperty(
-                                                      projectItemId,
-                                                      (int)__VSHPROPID.VSHPROPID_ExtObject,
-                                                      out selectedObject));
-                }
-
-                dynamic dyn = selectedObject;
-                items = (ProjectItems)(dyn.ProjectItems);
-            }
-            catch(Exception)
-            { 
-                // assuming solution was clicked
-            }
-
-
-            try
-            {
-
-                DirectoryInfo folder = GetFolder();
+                DirectoryInfo folder = GetFolder(basePath, destination);
                 if (folder == null)
                     return;
 
-                bool isSolutionFolder = false;
-                const string folderKindVirtual  = @"{6BB5F8F0-4483-11D3-8BCF-00C04F8EC28C}";
-                
+                const string folderKindVirtual = @"{6BB5F8F0-4483-11D3-8BCF-00C04F8EC28C}";
+                const string folderKindPhysical = @"{6BB5F8EF-4483-11D3-8BCF-00C04F8EC28C}";
+
                 string folderKind = folderKindVirtual;
-                if (items != null)
+
+                bool isSolutionFolder = false;
+                switch (destination)
                 {
-                    items = items.AddFolder(folder.Name, folderKind).ProjectItems;
+                    case DestinationType.Solution:
+                        {
+                            Solution2 solution = (Solution2)dte.Solution;
+                            rootItems = solution.AddSolutionFolder(folder.Name).ProjectItems;
+                            isSolutionFolder = true;
+                        }
+                        break;
+
+                    case DestinationType.SolutionFolder:
+                        {
+                            var solutionFolder = (SolutionFolder)((Project)rootItems.Parent).Object;
+                            rootItems = solutionFolder.AddSolutionFolder(folder.Name).ProjectItems;
+                            isSolutionFolder = true;
+                        }
+                        break;
+
+                    case DestinationType.Project:
+                        {
+                            if (rootItems != null)
+                            {
+                                try
+                                {
+                                    // try virtual folder first (C/C++ projects)
+                                    rootItems = rootItems.AddFolder(folder.Name, folderKind).ProjectItems;
+                                }
+                                catch(Exception)
+                                {
+                                    // try physical folder (.net language projects)
+                                    folderKind = folderKindPhysical;
+                                    rootItems = rootItems.AddFolder(folder.Name, folderKind).ProjectItems;
+                                }
+                            }
+                        }
+                        break;
                 }
-                else
-                {
-                    isSolutionFolder = true;
-                    Solution2 solution = (Solution2)dte.Solution;
-                    items = solution.AddSolutionFolder(folder.Name).ProjectItems;
-                }
 
-                dte.StatusBar.Text = $"Creating Folders for {folder.Name}";
-
-                IncludeFiles(folder, items, isSolutionFolder, folderKind);
-
-                dte.StatusBar.Text = $"Created Folders for {folder.Name}";
+                IncludeFiles(folder, rootItems, isSolutionFolder, folderKind, folder.Name);
             }
             catch (Exception ex)
             {
@@ -178,14 +265,17 @@ namespace CeciliaSharp.FolderToSolutionFolder
             }
         }
 
-        private static void IncludeFiles(DirectoryInfo folder, ProjectItems items, bool isSolutionFolder, string folderKind)
+        private static void IncludeFiles(DirectoryInfo folder, ProjectItems items, bool isSolutionFolder, string folderKind, string recursiveFolderName)
         {
+            dte.StatusBar.Text = $"Creating Folder {recursiveFolderName}";
+
             foreach (var item in folder.GetFileSystemInfos())
             {
                 if (item is FileInfo)
                 {
                     try
                     {
+                        dte.StatusBar.Text = $"Adding file: {item.FullName}";
                         items.AddFromFile(item.FullName);
                     }
                     catch(Exception)
@@ -199,27 +289,27 @@ namespace CeciliaSharp.FolderToSolutionFolder
                     {
                         var solutionFolder = (SolutionFolder)((Project)items.Parent).Object;
                         var newSolutionFolder = solutionFolder.AddSolutionFolder(item.Name).ProjectItems;
-                        IncludeFiles(info, newSolutionFolder, isSolutionFolder, folderKind);
+                        IncludeFiles(info, newSolutionFolder, isSolutionFolder, folderKind, recursiveFolderName + @"\" + item.Name);
                     }
                     else
                     {
                         var newItems = items.AddFolder(item.Name, folderKind).ProjectItems;
-                        IncludeFiles(info, newItems, isSolutionFolder, folderKind);
+                        IncludeFiles(info, newItems, isSolutionFolder, folderKind, recursiveFolderName + @"\" + item.Name);
                     }
                 }
             }
+
+            dte.StatusBar.Text = $"Created Folder {recursiveFolderName}";
         }
 
-        private DirectoryInfo GetFolder()
+        private DirectoryInfo GetFolder(string basePath, DestinationType destination)
         {
-            Solution2 sol2 = (Solution2)dte.Solution;
-            var solutionPath = Path.GetDirectoryName(sol2.FullName);
 
             using (var dialog = new CommonOpenFileDialog
             {
                 IsFolderPicker = true,
                 Title = "Select a folder. The folder you pick will be created as a solution folder and containing files will be added to it.",
-                InitialDirectory = solutionPath
+                InitialDirectory = basePath
 
             })
 
